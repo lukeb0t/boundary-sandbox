100e6b4f9a46b12197bd40fc73ae5a6a4afdabfc
Luke McCleary <luke.mccleary@gmail.com>
2021-12-08 15:19:00 -0500

    sandbox

--- versions.tf
+++ versions.tf
@@ -2,11 +2,6 @@ terraform {
   required_providers {
     boundary = {
       source  = "hashicorp/boundary"
-      version = "1.0.5"
-    }
-  docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
     }
   vault  = {
     source = "hashicorp/vault"
